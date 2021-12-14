0f1c8920cb637d804223f5ec58756cf890efb921
aidenbradley <mraidenbradley@gmail.com>
2021-12-14 01:50:59 +0000

    Concern changes for http layer

--- tests/src/Kernel/Concerns/MakesHttpRequests.php
+++ tests/src/Kernel/Concerns/MakesHttpRequests.php
@@ -13,7 +13,9 @@ trait MakesHttpRequests
     /** @var \Symfony\Contracts\HttpClient\ResponseInterface */
     protected $response;
 
-    /** @phpstan-ignore-next-line */
+    /** @var null|bool */
+    protected $requestIsAjax = null;
+
     public function get(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
     {
         return $this->handleRequest(
@@ -21,7 +23,6 @@ trait MakesHttpRequests
         );
     }
 
-    /** @phpstan-ignore-next-line */
     public function post(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
     {
         return $this->handleRequest(
@@ -29,7 +30,6 @@ trait MakesHttpRequests
         );
     }
 
-    /** @phpstan-ignore-next-line */
     public function put(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
     {
         return $this->handleRequest(
@@ -37,7 +37,6 @@ trait MakesHttpRequests
         );
     }
 
-    /** @phpstan-ignore-next-line */
     public function delete(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
     {
         return $this->handleRequest(
@@ -45,38 +44,11 @@ trait MakesHttpRequests
         );
     }
 
-    /** @phpstan-ignore-next-line */
-    public function ajaxDelete(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
+    public function ajax(): self
     {
-        return $this->ajax($uri, 'DELETE', $parameters, $cookies, $files, $server, $content);
-    }
+        $this->requestIsAjax = true;
 
-    /** @phpstan-ignore-next-line */
-    public function ajaxPatch(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
-    {
-        return $this->ajax($uri, 'PATCH', $parameters, $cookies, $files, $server, $content);
-    }
-
-    /** @phpstan-ignore-next-line */
-    public function ajaxPost(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
-    {
-        return $this->ajax($uri, 'POST', $parameters, $cookies, $files, $server, $content);
-    }
-
-    /** @phpstan-ignore-next-line */
-    public function ajaxGet(string $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
-    {
-        return $this->ajax($uri, 'GET', $parameters, $cookies, $files, $server, $content);
-    }
-
-    /** @phpstan-ignore-next-line */
-    public function ajax(string $uri, string $method = 'POST', $parameters = [], $cookies = [], $files = [], $server = [], $content = null): self
-    {
-        $request = Request::create($uri, $method, $parameters, $cookies, $files, $server, $content);
-
-        $request->headers->set('X-Requested-With', 'XMLHttpRequest');
-
-        return $this->handleRequest($request);
+        return $this;
     }
 
     public function handleRequest(Request $request): self
@@ -88,6 +60,12 @@ trait MakesHttpRequests
 
         $this->request->setSession($this->container->get('session'));
 
+        if ($this->requestIsAjax !== null) {
+            $request->headers->set('X-Requested-With', 'XMLHttpRequest');
+
+            $this->requestIsAjax = null;
+        }
+
         $this->response = $this->container->get('http_kernel')->handle($this->request);
 
         return $this;
@@ -96,14 +74,6 @@ trait MakesHttpRequests
     public function getRequest(): Request
     {
         return $this->request;
-    }
-
-    /** @return mixed */
-    public function getJson(string $uri)
-    {
-        return (array)json_decode(
-            $this->ajaxGet($uri)->response->getContent()
-        );
     }
 
     public function assertOk(): void
@@ -134,6 +104,20 @@ trait MakesHttpRequests
     public function assertMethodNotAllowed(): void
     {
         $this->assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $this->response->getStatusCode());
+    }
+
+    public function assertJsonContent(array $json): void
+    {
+        $this->assertEquals($json, json_decode($this->response->getContent()));
+    }
+
+    public function assertJsonContentContains(array $json): void
+    {
+        $decodedResponse = json_decode($this->response->getContent());
+
+        foreach ($json as $key => $value) {
+            $this->assertEquals($value, $decodedResponse->{$key});
+        }
     }
 
     public function followRedirect(): self
