53534fad806663eb81ba071f7f2088a2852e993e
aidenbradley <mraidenbradley@gmail.com>
2021-12-15 01:52:51 +0000

    Swap out for constants to make tests more readable

--- tests/src/Kernel/TestResponseTest.php
+++ tests/src/Kernel/TestResponseTest.php
@@ -11,277 +11,277 @@ class TestResponseTest extends UnitTestCase
     /** @test */
     public function assert_continue(): void
     {
-        $this->createMockResponse(100)->assertContinue();
+        $this->createMockResponse(Response::HTTP_CONTINUE)->assertContinue();
     }
 
     /** @test */
     public function assert_switching_protocols(): void
     {
-        $this->createMockResponse(101)->assertSwitchingProtocols();
+        $this->createMockResponse(Response::HTTP_SWITCHING_PROTOCOLS)->assertSwitchingProtocols();
     }
 
     /** @test */
     public function assert_processing(): void
     {
-        $this->createMockResponse(102)->assertProcessing();
+        $this->createMockResponse(Response::HTTP_PROCESSING)->assertProcessing();
     }
 
     /** @test */
     public function assert_early_hints(): void
     {
-        $this->createMockResponse(103)->assertEarlyHints();
+        $this->createMockResponse(Response::HTTP_EARLY_HINTS)->assertEarlyHints();
     }
 
     /** @test */
     public function assert_ok(): void
     {
-        $this->createMockResponse(200)->assertOk();
+        $this->createMockResponse(Response::HTTP_OK)->assertOk();
     }
 
     /** @test */
     public function assert_created(): void
     {
-        $this->createMockResponse(201)->assertCreated();
+        $this->createMockResponse(Response::HTTP_CREATED)->assertCreated();
     }
 
     /** @test */
     public function assert_accepted(): void
     {
-        $this->createMockResponse(202)->assertAccepted();
+        $this->createMockResponse(Response::HTTP_ACCEPTED)->assertAccepted();
     }
 
     /** @test */
     public function assert_non_authoritative_information(): void
     {
-        $this->createMockResponse(203)->assertNonAuthoritativeInformation();
+        $this->createMockResponse(Response::HTTP_NON_AUTHORITATIVE_INFORMATION)->assertNonAuthoritativeInformation();
     }
 
     /** @test */
     public function assert_no_content(): void
     {
-        $this->createMockResponse(204)->assertNoContent();
+        $this->createMockResponse(Response::HTTP_NO_CONTENT)->assertNoContent();
     }
 
     /** @test */
     public function assert_reset_content(): void
     {
-        $this->createMockResponse(205)->assertResetContent();
+        $this->createMockResponse(Response::HTTP_RESET_CONTENT)->assertResetContent();
     }
 
     /** @test */
     public function assert_partial_content(): void
     {
-        $this->createMockResponse(206)->assertPartialContent();
+        $this->createMockResponse(Response::HTTP_PARTIAL_CONTENT)->assertPartialContent();
     }
 
     /** @test */
     public function assert_multi_status(): void
     {
-        $this->createMockResponse(207)->assertMultiStatus();
+        $this->createMockResponse(Response::HTTP_MULTI_STATUS)->assertMultiStatus();
     }
 
     /** @test */
     public function assert_already_reported(): void
     {
-        $this->createMockResponse(208)->assertAlreadyReported();
+        $this->createMockResponse(Response::HTTP_ALREADY_REPORTED)->assertAlreadyReported();
     }
 
     /** @test */
     public function assert_im_used(): void
     {
-        $this->createMockResponse(226)->assertImUsed();
+        $this->createMockResponse(Response::HTTP_IM_USED)->assertImUsed();
     }
 
     /** @test */
     public function assert_multiple_choices(): void
     {
-        $this->createMockResponse(300)->assertMultipleChoices();
+        $this->createMockResponse(Response::HTTP_MULTIPLE_CHOICES)->assertMultipleChoices();
     }
 
     /** @test */
     public function assert_moved_permanently(): void
     {
-        $this->createMockResponse(301)->assertMovedPermanently();
+        $this->createMockResponse(Response::HTTP_MOVED_PERMANENTLY)->assertMovedPermanently();
     }
 
     /** @test */
     public function assert_found(): void
     {
-        $this->createMockResponse(302)->assertFound();
+        $this->createMockResponse(Response::HTTP_FOUND)->assertFound();
     }
 
     /** @test */
     public function assert_see_other(): void
     {
-        $this->createMockResponse(303)->assertSeeOther();
+        $this->createMockResponse(Response::HTTP_SEE_OTHER)->assertSeeOther();
     }
 
     /** @test */
     public function assert_not_modified(): void
     {
-        $this->createMockResponse(304)->assertNotModified();
+        $this->createMockResponse(Response::HTTP_NOT_MODIFIED)->assertNotModified();
     }
 
     /** @test */
     public function assert_use_proxy(): void
     {
-        $this->createMockResponse(305)->assertUseProxy();
+        $this->createMockResponse(Response::HTTP_USE_PROXY)->assertUseProxy();
     }
 
     /** @test */
     public function assert_reserved(): void
     {
-        $this->createMockResponse(306)->assertReserved();
+        $this->createMockResponse(Response::HTTP_RESERVED)->assertReserved();
     }
 
     /** @test */
     public function assert_temporary_redirect(): void
     {
-        $this->createMockResponse(307)->assertTemporaryRedirect();
+        $this->createMockResponse(Response::HTTP_TEMPORARY_REDIRECT)->assertTemporaryRedirect();
     }
 
     /** @test */
     public function assert_permanently_redirect(): void
     {
-        $this->createMockResponse(308)->assertPermanentlyRedirect();
+        $this->createMockResponse(Response::HTTP_PERMANENTLY_REDIRECT)->assertPermanentlyRedirect();
     }
 
     /** @test */
     public function assert_bad_request(): void
     {
-        $this->createMockResponse(400)->assertBadRequest();
+        $this->createMockResponse(Response::HTTP_BAD_REQUEST)->assertBadRequest();
     }
 
     /** @test */
     public function assert_unauthorized(): void
     {
-        $this->createMockResponse(401)->assertUnauthorized();
+        $this->createMockResponse(Response::HTTP_UNAUTHORIZED)->assertUnauthorized();
     }
 
     /** @test */
     public function assert_payment_required(): void
     {
-        $this->createMockResponse(402)->assertPaymentRequired();
+        $this->createMockResponse(Response::HTTP_PAYMENT_REQUIRED)->assertPaymentRequired();
     }
 
     /** @test */
     public function assert_forbidden(): void
     {
-        $this->createMockResponse(403)->assertForbidden();
+        $this->createMockResponse(Response::HTTP_FORBIDDEN)->assertForbidden();
     }
 
     /** @test */
     public function assert_not_found(): void
     {
-        $this->createMockResponse(404)->assertNotFound();
+        $this->createMockResponse(Response::HTTP_NOT_FOUND)->assertNotFound();
     }
 
     /** @test */
     public function assert_method_not_allowed(): void
     {
-        $this->createMockResponse(405)->assertMethodNotAllowed();
+        $this->createMockResponse(Response::HTTP_METHOD_NOT_ALLOWED)->assertMethodNotAllowed();
     }
 
     /** @test */
     public function assert_not_acceptable(): void
     {
-        $this->createMockResponse(406)->assertNotAcceptable();
+        $this->createMockResponse(Response::HTTP_NOT_ACCEPTABLE)->assertNotAcceptable();
     }
 
     /** @test */
     public function assert_proxy_authentication_required(): void
     {
-        $this->createMockResponse(407)->assertProxyAuthenticationRequired();
+        $this->createMockResponse(Response::HTTP_PROXY_AUTHENTICATION_REQUIRED)->assertProxyAuthenticationRequired();
     }
 
     /** @test */
     public function assert_request_timeout(): void
     {
-        $this->createMockResponse(408)->assertRequestTimeout();
+        $this->createMockResponse(Response::HTTP_REQUEST_TIMEOUT)->assertRequestTimeout();
     }
 
     /** @test */
     public function assert_conflict(): void
     {
-        $this->createMockResponse(409)->assertConflict();
+        $this->createMockResponse(Response::HTTP_CONFLICT)->assertConflict();
     }
 
     /** @test */
     public function assert_gone(): void
     {
-        $this->createMockResponse(410)->assertGone();
+        $this->createMockResponse(Response::HTTP_GONE)->assertGone();
     }
 
     /** @test */
     public function assert_length_required(): void
     {
-        $this->createMockResponse(411)->assertLengthRequired();
+        $this->createMockResponse(Response::HTTP_LENGTH_REQUIRED)->assertLengthRequired();
     }
 
     /** @test */
     public function assert_precondition_failed(): void
     {
-        $this->createMockResponse(412)->assertPreconditionFailed();
+        $this->createMockResponse(Response::HTTP_PRECONDITION_FAILED)->assertPreconditionFailed();
     }
 
     /** @test */
     public function assert_request_entity_too_large(): void
     {
-        $this->createMockResponse(413)->assertRequestEntityTooLarge();
+        $this->createMockResponse(Response::HTTP_REQUEST_ENTITY_TOO_LARGE)->assertRequestEntityTooLarge();
     }
 
     /** @test */
     public function assert_request_uri_too_long(): void
     {
-        $this->createMockResponse(414)->assertRequestUriTooLong();
+        $this->createMockResponse(Response::HTTP_REQUEST_URI_TOO_LONG)->assertRequestUriTooLong();
     }
 
     /** @test */
     public function assert_unsupported_media_type(): void
     {
-        $this->createMockResponse(415)->assertUnsupportedMediaType();
+        $this->createMockResponse(Response::HTTP_UNSUPPORTED_MEDIA_TYPE)->assertUnsupportedMediaType();
     }
 
     /** @test */
     public function assert_request_range_not_satisfiable(): void
     {
-        $this->createMockResponse(416)->assertRequestRangeNotSatisfiable();
+        $this->createMockResponse(Response::HTTP_REQUESTED_RANGE_NOT_SATISFIABLE)->assertRequestRangeNotSatisfiable();
     }
 
     /** @test */
     public function assert_expectation_failed(): void
     {
-        $this->createMockResponse(417)->assertExpectationFailed();
+        $this->createMockResponse(Response::HTTP_EXPECTATION_FAILED)->assertExpectationFailed();
     }
 
     /** @test */
     public function assert_i_am_a_teapot(): void
     {
-        $this->createMockResponse(418)->assertImATeapot();
+        $this->createMockResponse(Response::HTTP_I_AM_A_TEAPOT)->assertImATeapot();
     }
 
     /** @test */
     public function assert_misdirected_request(): void
     {
-        $this->createMockResponse(421)->assertMisdirectedRequest();
+        $this->createMockResponse(Response::HTTP_MISDIRECTED_REQUEST)->assertMisdirectedRequest();
     }
 
     /** @test */
     public function assert_unprocessable_entity(): void
     {
-        $this->createMockResponse(422)->assertUnprocessable();
+        $this->createMockResponse(Response::HTTP_UNPROCESSABLE_ENTITY)->assertUnprocessable();
     }
 
     /** @test */
     public function assert_locked(): void
     {
-        $this->createMockResponse(423)->assertLocked();
+        $this->createMockResponse(Response::HTTP_LOCKED)->assertLocked();
     }
 
     /** @test */
     public function assert_failed_dependency(): void
     {
-        $this->createMockResponse(424)->assertFailedDependency();
+        $this->createMockResponse(Response::HTTP_FAILED_DEPENDENCY)->assertFailedDependency();
     }
 
     /** @test */
