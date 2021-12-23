ce2b017d3cc45f7e1e5c30d90808e86ed9229c27
aidenbradley <mraidenbradley@gmail.com>
2021-12-23 21:29:54 +0000

    Adding an explanation to the EnableModuleKernelTestBase

--- tests/src/Kernel/Base/EnableModuleKernelTest.php
+++ tests/src/Kernel/Base/EnableModuleKernelTest.php
@@ -5,6 +5,11 @@ namespace Drupal\Tests\test_traits\Kernel\Base;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\Tests\test_traits\Kernel\Concerns\InstallsModules;
 
+/**
+ * This class will act as a base set of tests to test whether a module will work or not.
+ * The idea is to expand this class to provide default coverage for modules where all
+ * they need to do is extend this kernel test base and provide their module name
+ */
 abstract class EnableModuleKernelTest extends KernelTestBase
 {
     use InstallsModules;
