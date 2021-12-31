278eb304256515bfdbedfb0eb4415912bb4c2689
aidenbradley <mraidenbradley@gmail.com>
2021-12-31 01:26:18 +0000

    trying a refactor to a custom collection decorator

--- tests/src/Kernel/Testing/WithoutEventSubscribers.php
+++ tests/src/Kernel/Testing/WithoutEventSubscribers.php
@@ -2,13 +2,14 @@
 
 namespace Drupal\Tests\test_traits\Kernel\Testing;
 
+use Drupal\Tests\test_traits\Kernel\Testing\Collections\EventSubscriberCollection;
 use Drupal\Tests\test_traits\Kernel\Testing\Decorators\EventSubscriberDefinition as Definition;
 use Illuminate\Support\Collection;
 
 trait WithoutEventSubscribers
 {
     /** @var Definition[] */
-    private $definitions;
+    private $definitionCollection;
 
     /** @var bool */
     private $ignoreAllSubscribers = false;
@@ -36,7 +37,7 @@ trait WithoutEventSubscribers
         if ($subscribers === []) {
             $this->ignoreAllSubscribers = true;
 
-            $subscribers = $this->getDefinitions()->map->getServiceId()->toArray();
+            $subscribers = $this->getDefinitions()->getServiceIds();
         }
 
         return $this->ignore($subscribers);
@@ -85,7 +86,7 @@ trait WithoutEventSubscribers
     {
         parent::enableModules($modules);
 
-        $this->definitions = null;
+        $this->definitionCollection = null;
 
         if ($this->ignoreAllSubscribers) {
             $this->withoutSubscribers();
@@ -108,44 +109,28 @@ trait WithoutEventSubscribers
         return $this->removeDefinitions();
     }
 
-    /** @return Definition[] */
-    private function getDefinitions(): Collection
+    private function getDefinitions(): EventSubscriberCollection
     {
-        if (isset($this->definitions) === false) {
-            $eventSubscribers = $this->container->findTaggedServiceIds('event_subscriber');
-
-            $this->definitions = collect($eventSubscribers)->keys()->map(function (string $serviceId) {
-                return $this->container->getDefinition($serviceId);
-            })->mapInto(Definition::class);
+        if (isset($this->definitionCollection) === false) {
+            $this->definitionCollection = EventSubscriberCollection::create($this->container);
         }
 
-        return $this->definitions;
+        return $this->definitionCollection;
     }
 
     private function removeDefinitions(): self
     {
         foreach ($this->ignoredSubscribers as $listener) {
-            if ($this->container->has($listener)) {
-                $this->container->removeDefinition($listener);
-
-                continue;
-            }
-
-            if ($this->container->get('module_handler')->moduleExists($listener)) {
-                $this->getDefinitions()->filter(function (Definition $definition) use ($listener) {
+            $this->getDefinitions()
+                ->removeDefinitionsWhere(function (Definition $definition) use ($listener) {
+                    return $definition->getServiceId() === $listener;
+                })
+                ->removeDefinitionsWhere(function (Definition $definition) use ($listener) {
                     return $definition->hasProvider() && $definition->providerIs($listener);
-                })->each(function (Definition $listener) {
-                    $this->container->removeDefinition($listener->getServiceId());
+                })
+                ->removeDefinitionsWhere(function (Definition $definition) use ($listener) {
+                    return $definition->classIs($listener) || $definition->subscribesTo($listener);
                 });
-
-                continue;
-            }
-
-            $this->getDefinitions()->filter(function (Definition $definition) use ($listener) {
-                return $definition->isClass($listener) || $definition->subscribesTo($listener);
-            })->each(function (Definition $listener) {
-                $this->container->removeDefinition($listener->getServiceId());
-            });
         }
 
         return $this;
