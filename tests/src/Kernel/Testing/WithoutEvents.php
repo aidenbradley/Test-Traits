3df30806a535ff834f0c0d0ca209389c6f9258a7
aidenbradley <mraidenbradley@gmail.com>
2022-01-02 02:05:26 +0000

    Mock the event dispatcher and collect any dispatched events for asserting against

--- tests/src/Kernel/Testing/WithoutEvents.php
+++ tests/src/Kernel/Testing/WithoutEvents.php
@@ -2,10 +2,45 @@
 
 namespace Drupal\Tests\test_traits\Kernel\Testing;
 
+use Prophecy\Argument;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
 trait WithoutEvents
 {
+    /** @var array */
+    public $firedEvents;
+
+    /** Mock the event dispatcher. All dispatched events are collected */
     public function withoutEvents(): self
     {
+        $dispatcher = $this->prophesize(EventDispatcherInterface::class);
+
+        $dispatcher->dispatch(Argument::any(), Argument::type('string'))->will([
+            $this, 'registerDispatchedEvent'
+        ]);
+
+        $this->container->set('event_dispatcher', $dispatcher->reveal());
+
         return $this;
+    }
+
+    public function assertEventFired(string $event): self
+    {
+        $assertEventName = collect($this->firedEvents)->keys()->filter(function(string $eventName) use ($event) {
+            return $eventName === $event;
+        });
+
+        $assertEventClass = collect($this->firedEvents)->values()->filter(function($eventName) use ($event) {
+            return get_class($eventName) === $event;
+        });
+
+        $this->assertTrue($assertEventName->isNotEmpty() || $assertEventClass->isNotEmpty());
+
+        return $this;
+    }
+
+    public function registerDispatchedEvent($arguments): void
+    {
+        $this->firedEvents[$arguments[1]] = $arguments[0];
     }
 }
--- tests/src/Kernel/WithoutEventsTest.php
+++ tests/src/Kernel/WithoutEventsTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\test_traits\Kernel;
 
+use Drupal\Component\EventDispatcher\Event;
 use Drupal\Tests\test_traits\Kernel\Testing\WithoutEvents;
 use Drupal\Tests\token\Kernel\KernelTestBase;
 
@@ -13,5 +14,10 @@ class WithoutEventsTest extends KernelTestBase
     public function without_events(): void
     {
         $this->withoutEvents();
+
+        $this->container->get('event_dispatcher')->dispatch(new Event(), 'test_event');
+
+        $this->assertEventFired('test_event');
+        $this->assertEventFired(Event::class);
     }
 }
