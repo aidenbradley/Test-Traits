1601c6f02ca5a4b3714da542729b66043e5f133f
aidenbradley <mraidenbradley@gmail.com>
2022-01-16 20:50:36 +0000

    Trying to test batching

---
+++ tests/modules/test_traits_batch/src/Controller/DisableAllUsersBatch.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\test_traits_batch\Controller;
+
+use Drupal\Core\Batch\BatchBuilder;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\user\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class DisableAllUsersBatch implements ContainerInjectionInterface
+{
+    /** @var EntityStorageInterface */
+    private $userStorage;
+
+    public static function create(ContainerInterface $container): self
+    {
+        return new self(
+            $container->get('entity_type.manager')->getStorage('user')
+        );
+    }
+
+    public function __construct(EntityStorageInterface $userStorage)
+    {
+        $this->userStorage = $userStorage;
+    }
+
+    public function prepareBatch(): void
+    {
+        $builder = new BatchBuilder();
+        $builder->setTitle('Disable Users')
+            ->setInitMessage('Disabling users. Processed @current.')
+            ->setProgressMessage('Processed @current out of @total.')
+            ->setErrorMessage('Batch has encountered an error.');
+
+        /** @var \Drupal\user\Entity\User $user */
+        foreach ($this->userStorage->loadMultiple() as $user) {
+            $builder->addOperation([$this, 'disableUser'], [$user]);
+        }
+
+        batch_set($builder->toArray());
+    }
+
+    public function prepareBatchAndProcess(): RedirectResponse
+    {
+        $this->prepareBatch();
+
+        return batch_process('/');
+    }
+
+    public function disableUser(User $user)
+    {
+        $user->status->value = 0;
+
+        $user->save();
+    }
+}
---
+++ tests/src/Kernel/InteractsWithBatchesTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\Tests\test_traits\Kernel;
+
+use Drupal\Core\Url;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\test_traits\Kernel\Testing\Concerns\MakesHttpRequests;
+
+class InteractsWithBatchesTest extends KernelTestBase
+{
+    use MakesHttpRequests;
+
+    protected static $modules = [
+        'system',
+        'user',
+        'test_traits_batch',
+    ];
+
+    /** @var \Drupal\user\UserStorage */
+    private $userStorage;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->installEntitySchema('user');
+        $this->installSchema('system', 'sequences');
+
+        $this->userStorage = $this->container->get('entity_type.manager')->getStorage('user');
+    }
+
+    /** @test */
+    public function process_batch(): void
+    {
+        $this->createDisabledUser('disabled_user_one')
+            ->createDisabledUser('disabled_user_two')
+            ->createDisabledUser('disabled_user_three');
+
+        $this->get($this->route('disable_all_users.prepare_and_process_batch'));
+
+        $disabledUserOne = $this->userStorage->load(1);
+        $this->assertEquals(0, $disabledUserOne->status->value);
+
+        $disabledUserTwo = $this->userStorage->load(2);
+        $this->assertEquals(0, $disabledUserTwo->status->value);
+
+        $disabledUserThree = $this->userStorage->load(3);
+        $this->assertEquals(0, $disabledUserThree->status->value);
+    }
+
+    private function createDisabledUser(string $name): self
+    {
+        $this->userStorage->create([
+            'status' => 1,
+            'mail' => $name . '@example.com',
+            'name' => $name,
+        ])->save();
+
+        return $this;
+    }
+
+    private function route(string $route, array $parameters = [], array $options = []): string
+    {
+        return Url::fromRoute(...func_get_args())->toString(true)->getGeneratedUrl();
+    }
+}
