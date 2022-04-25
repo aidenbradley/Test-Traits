bb31e89e960d715e233bd370b2e18c7238171f45
aidenbradley <mraidenbradley@gmail.com>
2022-04-25 21:52:23 +0100

    Adding trait and test class for InstallsImageStyles

--- tests/src/Kernel/Testing/Concerns/InstallsImageStyles.php
+++ tests/src/Kernel/Testing/Concerns/InstallsImageStyles.php
@@ -6,14 +6,34 @@ trait InstallsImageStyles
 {
     use InstallsExportedConfig;
 
+    /** @var bool */
+    private $setupImageStyleDependencies = false;
+
     /** @param string|array $imageStyles */
     public function installImageStyles($imageStyles): self
     {
+        $this->prepareImageStyleDependencies();
+
         foreach ((array) $imageStyles as $imageStyle) {
             $this->installExportedConfig([
                 'image.style.' . $imageStyle,
             ]);
         }
+
+        return $this;
+    }
+
+    private function prepareImageStyleDependencies(): self
+    {
+        if ($this->setupImageStyleDependencies === false) {
+            $this->enableModules([
+                'image',
+            ]);
+
+            $this->installEntitySchema('image_style');
+        }
+
+        $this->setupImageStyleDependencies = true;
 
         return $this;
     }
