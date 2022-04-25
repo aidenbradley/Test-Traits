32c4ff6a0228beef1ddf429c07b36650a395db19
aidenbradley <mraidenbradley@gmail.com>
2022-04-25 22:42:26 +0100

    Adding trait and test class for InstallsRoles

---
+++ tests/src/Kernel/InstallsRolesTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\Tests\test_traits\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\test_traits\Kernel\Testing\Concerns\InstallsRoles;
+use Drupal\user\Entity\Role;
+
+class InstallsRolesTest extends KernelTestBase
+{
+    use InstallsRoles;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->setConfigDirectory(__DIR__ . '/__fixtures__/config/sync/roles');
+    }
+
+    /** @test */
+    public function installing_role_sets_dependencies(): void
+    {
+        $this->assertFalse($this->container->get('module_handler')->moduleExists('user'));
+
+        $entityTypeDefinitions = $this->container->get('entity_type.manager')->getDefinitions();
+        $this->assertArrayNotHasKey('user_role', $entityTypeDefinitions);
+
+        $this->installRoles('editor');
+
+        $this->assertTrue($this->container->get('module_handler')->moduleExists('user'));
+
+        $entityTypeDefinitions = $this->container->get('entity_type.manager')->getDefinitions();
+        $this->assertArrayHasKey('user_role', $entityTypeDefinitions);
+    }
+
+    /** @test */
+    public function installs_single_role(): void
+    {
+        $this->enableModules([
+            'system',
+            'user',
+        ]);
+        $this->installEntitySchema('user_role');
+
+        $roleStorage = $this->container->get('entity_type.manager')->getStorage('user_role');
+
+        $this->assertEmpty($roleStorage->loadMultiple());
+
+        $this->installRoles('editor');
+
+        $roles = $roleStorage->loadMultiple();
+
+        $this->assertNotEmpty($roles);
+
+        $editor = reset($roles);
+
+        $this->assertEquals('editor', $editor->id());
+    }
+
+    /** @test */
+    public function installs_multiple_roles(): void
+    {
+        $this->enableModules([
+            'system',
+            'user',
+        ]);
+        $this->installEntitySchema('user_role');
+
+        $roleStorage = $this->container->get('entity_type.manager')->getStorage('user_role');
+
+        $this->assertEmpty($roleStorage->loadMultiple());
+
+        $this->installRoles([
+            'editor',
+            'writer',
+        ]);
+
+        $this->assertInstanceOf(Role::class, $roleStorage->load('editor'));
+        $this->assertInstanceOf(Role::class, $roleStorage->load('writer'));
+    }
+}
--- tests/src/Kernel/Testing/Concerns/InstallsRoles.php
+++ tests/src/Kernel/Testing/Concerns/InstallsRoles.php
@@ -6,12 +6,33 @@ trait InstallsRoles
 {
     use InstallsExportedConfig;
 
+    /** @var bool */
+    private $setupRoleDependencies = false;
+
     /** @param string|array $roles */
     public function installRoles($roles): self
     {
+        $this->setupRoleDependencies();
+
         foreach ((array) $roles as $role) {
             $this->installExportedConfig('user.role.' . $role);
         }
+
+        return $this;
+    }
+
+    private function setupRoleDependencies(): self
+    {
+        if ($this->setupRoleDependencies === false) {
+            $this->enableModules([
+                'system',
+                'user',
+            ]);
+
+            $this->installEntitySchema('user_role');
+        }
+
+        $this->setupRoleDependencies = true;
 
         return $this;
     }
